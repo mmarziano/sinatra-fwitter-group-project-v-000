590303f147e887061173d9699f216deb1f1b0233
Monica Marziano <mmarziano1@gmail.com>
2019-06-12 23:13:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,8 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username])
     session[:user_id] = @user.id
-    binding.pry
-    
+    #binding.pry
+    erb :'/tweets/index'
   end
 
   get '/logout' do
